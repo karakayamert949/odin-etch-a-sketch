43bcac09d9bf88eaec1f555e3faf96658e83f72b
karakayamert949 <karakayamert949@gmail.com>
2023-10-02 18:23:29 +0300

    Add hover to color effect to grid squares

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let container = document.querySelector('.container');
 
 for (let i=0;i<256;i++){
     let cell = document.createElement('div');
-    cell.setAttribute('class','cell')
+    cell.setAttribute('class','cell');
+    cell.addEventListener('mouseover',()=>{cell.style.cssText = 'background-color: red;'});
     container.appendChild(cell);
 }
 
